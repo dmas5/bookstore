bd3da991c5a32ed27c9b44948a8e0d575045972b
dmas5 <artur.unt@gmail.com>
2023-09-25 19:06:21 +0300

    app

--- src/main/java/s23/project/BookstoreArtur/controller/BookController.java
+++ src/main/java/s23/project/BookstoreArtur/controller/BookController.java
@@ -7,9 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -63,23 +60,10 @@ public class BookController {
     public @ResponseBody List<Book> bookListRest () {
     	return (List<Book>) repository.findAll();
     }
-    //RESTful service to book by id
+    //RESTful service to find book by id
     @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
     public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
     	return repository.findById(bookId);
     }
-    //RESTful service to add new book
-	@PostMapping("books")
-	Book newBook(@RequestBody Book newBook) {
-		return repository.save(newBook);
-	}
-	//RESTful service to edit book
-	@PutMapping("/books/{id}")
-	Book editBook(@RequestBody Book editedBook, @PathVariable Long id) {
-		editedBook.setId(id);
-		return repository.save(editedBook);
-	}
-
-
 
 }
