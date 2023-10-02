4b81784b0447ddf1bf640cc6eadcd108c8807946
dmas5 <artur.unt@gmail.com>
2023-10-02 22:58:20 +0300

    app

--- src/main/java/s23/project/BookstoreArtur/BookstoreArturApplication.java
+++ src/main/java/s23/project/BookstoreArtur/BookstoreArturApplication.java
@@ -1,5 +1,6 @@
 package s23.project.BookstoreArtur;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +12,8 @@ import s23.project.BookstoreArtur.domain.Book;
 import s23.project.BookstoreArtur.domain.BookRepository;
 import s23.project.BookstoreArtur.domain.Category;
 import s23.project.BookstoreArtur.domain.CategoryRepository;
+import s23.project.BookstoreArtur.domain.User;
+import s23.project.BookstoreArtur.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreArturApplication {
@@ -22,7 +25,7 @@ public class BookstoreArturApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner bookDemo(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of books");
 			crepository.save(new Category("Fantasy"));
@@ -34,6 +37,11 @@ public class BookstoreArturApplication {
 			
 			repository.save(new Book("Sinuhe", "Mika Waltari", 1945,"457363-4756",10.95,crepository.findByName("History").get(0)));
 			repository.save(new Book("Seitseman veljesta", "Aleksis Kivi", 1870,"857555-822",7.95,crepository.findByName("History").get(0)));
+			
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 			
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
--- src/main/java/s23/project/BookstoreArtur/controller/BookController.java
+++ src/main/java/s23/project/BookstoreArtur/controller/BookController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,12 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	@Autowired
-	private CategoryRepository crepository; 
+	private CategoryRepository crepository;
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
 	
     @RequestMapping(value= {"/", "/booklist"})
     public String bookList(Model model) {	
@@ -43,6 +49,7 @@ public class BookController {
     }    
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
         return "redirect:../booklist";
